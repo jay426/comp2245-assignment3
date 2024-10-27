e87bea650a4fdc66d7d0e6c845553bcbecb21d8d
Javeesha Burton <javeesha14@gmail.com>
2024-10-27 15:21:15 -0400

    Added intial starter files

--- tic-tac-toe.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const squares = document.querySelectorAll("#board div");
-
-    squares.forEach(square => {
-        square.classList.add("square");
-    });
-});
